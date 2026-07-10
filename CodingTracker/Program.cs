043757e650ab276ed4b3e819f2b117a4dab4901f
felikshetalia <mccorsica@yahoo.com>
2026-07-10 13:43:37 +0200

    initial commit

---
+++ CodingTracker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Spectre.Console;
+
+namespace CodeReviews.Console.CodingTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.MarkupLine("[bold green]Welcome to the Coding Tracker![/]");
+            AnsiConsole.MarkupLine("[bold yellow]This application helps you track your coding sessions.[/]");
+            AnsiConsole.MarkupLine("[bold blue]Press any key to exit...[/]");
+            AnsiConsole.Console.Input.ReadKey(true);
+        }
+    }
+}
